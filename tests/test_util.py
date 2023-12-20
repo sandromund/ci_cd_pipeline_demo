a96d93f61f78aa66a41a6d74dbcc78ce1a84f4cb
Sandro Mund <sandro.mund@consistec.de>
2023-12-20 15:51:08 +0100

    Fixed lint

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,14 @@
+"""
+Test for the functions in src.util
+
+"""
+
 from src.util import addition
 
 
 def test_addition():
+    """
+    Test if the addition works.
+    :return: None
+    """
     assert addition(1, 2) == 3
