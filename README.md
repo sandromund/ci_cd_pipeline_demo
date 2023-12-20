c9d3a9abe0c6e95de378215f878ab9e9916a3068
Sandro Mund <sandro.mund@consistec.de>
2023-12-20 15:35:18 +0100

    Refactoring

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ci_cd_pipeline_demo
 Demo how to build a CI/CD pipeline with GitHub Actions 
+
+# Lint
+```shell
+pylint $(git ls-files '*.py')
+```
+
+# Test
+```shell
+pytest
+```
--- src/util.py
+++ src/util.py
@@ -1,2 +1,13 @@
-def addition(x, y):
-    return x + y
+"""
+Simple functions to test the CI/CD Actions
+"""
+
+def addition(number_x, number_y):
+    """
+    Add two numbers together.
+
+    :param number_x: first number
+    :param number_y: second number
+    :return: the sum of both numbers
+    """
+    return number_x + number_y
